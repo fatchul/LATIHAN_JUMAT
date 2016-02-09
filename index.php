8499ed77c7644b4a44028a4b59e589d79cae3789
fatchul <ftchulamin@gmail.com>
2016-02-09 18:45:17 +0700

    menambah file.

---
+++ index.php
@@ -0,0 +1,37 @@
+<form method="POST" action="proses.php">
+	<table>
+		<tr>
+			<td>
+				<?php echo "Username PHP"; ?>
+			</td>
+			<td>
+				:
+			</td>
+			<td>
+				<input type="text" name="username">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Password
+			</td>
+			<td>
+				:
+			</td>
+			<td>
+				<input type="password" name="pass">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				
+			</td>
+			<td>
+				
+			</td>
+			<td>
+				<input type="submit" value="Login">
+			</td>
+		</tr>
+	</table>
+</form>
